11e318c508e8406cff41124d1a25b0311da2dd82
remco jonk <remcojonk@hotmail.com>
2023-11-26 17:00:03 +0100

    created list of categories in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      img: "",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      img: "",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      img: "",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      img: "",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      img: "",
+    },
+  ]
+
   return (
-    <div className="App">
-      Hello world
+    <div className="categories-container">
+      {categories.map(({title, id}) => ( 
+      <div className="category-container" key={id}>
+            <div className="background-image"/>
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop now</p>
+            </div>
+        </div>
+       )
+      )}
     </div>
   );
 }
